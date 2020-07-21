c266d603cb196bdf15a2ade019f1396d9f36e16a
Anthony <Anton-bopoh@yandex.ru>
2020-07-21 12:17:48 +0300

    Initial Commit

---
+++ 6l_Anthony_Chechevichkin/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  6l_Anthony_Chechevichkin
+//
+//  Created by Katerina on 21.07.2020.
+//  Copyright © 2020 CHAS. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
